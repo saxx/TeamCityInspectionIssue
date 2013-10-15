b8a216e444d32649464ecf0bd00e821d2e22a046
Hannes Sachsenhofer <hannes.sachsenhofer@epunkt.net>
2013-10-15 15:18:05 +0200

    Added empty ASP.NET MVC project.

---
+++ TeamCityInspectionIssue/SomeClass.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+namespace TeamCityInspectionIssue
+{
+    public class SomeClass
+    {
+
+        [NotNull]
+        public object SomeMethod([CanBeNull]object someParameter)
+        {
+            return null;
+        }
+    }
+}
